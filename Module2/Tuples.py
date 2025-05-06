3dace2f2183a9aaa9e450f9a0bc377672307bee7
Leart-O <leartobertinca6@gmail.com>
2025-05-06 16:54:41 +0200

    ...

---
+++ Module2/Tuples.py
@@ -0,0 +1,5 @@
+#Colection of items oredered, immutable(unchangable(once created cannot be changed))
+word=("pizza","sandwich","rice","burger")
+print(word[0])
+# word[0]="pepperoni pizza"
+empty_tupple=()
