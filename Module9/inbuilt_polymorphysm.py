3f8fb16f35c85f96688bbb77c2b8e36036317d39
Leart-O <leartobertinca6@gmail.com>
2025-06-03 17:43:02 +0200

    ...

---
+++ Module9/inbuilt_polymorphysm.py
@@ -0,0 +1,13 @@
+string_length = len("Hello World")
+list_length =len([1,2,3,4,5,6])
+tuple_length = len((1,2,3,4,5))
+
+print(string_length)
+print(list_length)
+print(tuple_length)
+
+SumList = sum([1,2,3,4,5])
+SumTupple = sum((1,2,3))
+
+print(SumList)
+print(SumTupple)
