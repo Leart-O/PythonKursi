093a2e066105b904acb8b71d5a0e72fb3c2530c3
Leart-O <leartobertinca6@gmail.com>
2025-06-12 17:17:32 +0200

    Finished a challenge on oop and superclasses, including properties and methods

--- Module10/challange.py
+++ Module10/challange.py
@@ -11,7 +11,8 @@ class DigitalSchool:
 
     @name.setter
     def name(self, name):
-        self.__name = name
+        self.name = name
+        self.__name = self.name
 
     @property
     def city(self):
@@ -38,6 +39,37 @@ class DigitalSchool:
         self.__courses = courses
 
     def show_school_info(self):
-        return contact_info={
-            "Digital School":"049123456"
+        return {
+            "name":self.__name,
+            "city":self.__city,
+            "state":self.__state,
+            "courses":self.__courses
         }
+    def organize_hackathon(self):
+        print("Organizing a generic hackathon...")
+
+#define a subclass called DS Prishtina where it has a private attribute called student_number and 2 methods organize hackathon and SCF
+class DS_Prishtina(DigitalSchool):
+    def __init__(self, name, city, state, courses, student_number):
+        super().__init__(name,city,state,courses)
+        self.__student_number=student_number
+    def SCF(self):
+        print("First Edition")
+    def organize_hackathon(self):
+        print("We are doing a beta analysis hackathon")
+
+
+class DS_Ferizaj(DigitalSchool):
+    def __init__(self, name, city, state, courses, student_number):
+        super().__init__(name,city,state,courses)
+        self.__student_number=student_number
+    def Internship(self):
+        print("Looking for interns!")
+    def organize_hackathon(self):
+        print("We are doing a hackathon!")
+
+prishtina_obj=DS_Prishtina("DS_Prishtina", "Prishtina", "Kosova", ["PHP","Python","Java"], 3000)
+print(prishtina_obj.show_school_info())
+
+ferizaj_obj=DS_Ferizaj("DS_Ferizaj", "Ferizaj", "Kosova", ["HTML", "CSS", "JavaScript"], 2400)
+ferizaj_obj.Internship()
