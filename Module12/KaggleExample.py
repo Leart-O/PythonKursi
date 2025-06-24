55a9eaba8cb2e89726599c2b88a6da1a8e9a3d3a
Leart-O <leartobertinca6@gmail.com>
2025-06-24 17:45:54 +0200

    Displaying data in a more visual way using matplotlib

--- Module12/KaggleExample.py
+++ Module12/KaggleExample.py
@@ -1,4 +1,6 @@
 import pandas as pd
+from pandas.compat.numpy.function import SORT_DEFAULTS
+
 df = pd.read_csv("avgIQpercountry.csv")
 print(df.info()) #we see the columns of this dataset
 print(df.head()) #we see the first 5 rows
@@ -18,3 +20,18 @@ print(null_count)
 #removing duplicates
 df.drop_duplicates(keep="first",inplace=True)
 df["Population - 2023"]=df["Population - 2023"].apply(lambda x: float(x.replace(",","")))
+
+#find the avg of a continent
+avg_iq_per_continent=df.groupby('Continent')["Average IQ"].mean()
+
+avg_iq_per_continent=avg_iq_per_continent.sort_values(ascending=False)
+print(avg_iq_per_continent)
+
+#calculate nobel prizes by country. And show countries only with more than 1 nobel
+# you have to use Group By, Sum and sort values
+
+nobel_per_country=df.groupby('Country')["Nobel Prices"].sum()
+sorted_nobel_per_country=nobel_per_country.sort_values(ascending=False)
+sorted_nobel_per_country=sorted_nobel_per_country[sorted_nobel_per_country!=0]
+print(sorted_nobel_per_country)
+
---
+++ Module14/Example1.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from PIL.Image import FILTERED
+
+#getting data from file
+df=pd.read_csv("avgIQpercountry.csv")
+
+#filtering the data based on average iq level
+filtered_DF=df[df["Average IQ"]>=100]
+
+#sorting data
+filtered_DF=filtered_DF.sort_values(by="Average IQ", ascending=False)
+
+#test the data
+print(filtered_DF)
+
+#krijimi i figures ku do te shfaqen te dhenat
+plt.figure(figsize=(14,8))
+
+#grafikat si shtylla ku definohet qkae eshte ne x dhe ne y
+bars=plt.bar(filtered_DF["Country"],filtered_DF["Average IQ"], color="skyblue")
+
+plt.title("Average IQ by Country (IQ>100)", fontsize=14)
+
+plt.xlabel("Country", fontsize=14)
+
+plt.ylabel("Average", fontsize=14)
+
+plt.bar_label(bars,fmt="%.2f", fontsize=10, color="black")
+
+plt.tight_layout()
+
+plt.show()
