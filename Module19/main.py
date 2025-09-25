0aa6b885f8f5cb73455c1540326770676d355d28
Leart Obertinca <leartobertinca6@gmail.com>
2025-09-25 17:42:00 +0200

    ...

--- Module19/main.py
+++ Module19/main.py
@@ -29,6 +29,58 @@ with st.sidebar.form("my_form"):
             "Price": [new_price],
             "Year": [new_year]
         }
-        book = pd.concat([pd.DataFrame(new_data), book], ignore_index=True)
+        book = pd.concat([pd.DataFrame(new_data, index=[0]), book], ignore_index=True)
         book.to_csv("bestsellers_with_categories_2022_03_27.csv", index=False)
         st.sidebar.success("Book added successfully!")
+
+st.sidebar.header("Filter Options")
+selected_author = st.sidebar.selectbox("Select Author", ["ALL"] + list(book['Author'].unique()))
+selected_year = st.sidebar.selectbox("Select Year", ["ALL"] + list(book['Year'].unique()))
+selected_genre = st.sidebar.selectbox("Select Genre", ["ALL"] + list(book['Genre'].unique()))
+min_rating = st.sidebar.slider("Minimum User Rating", 0.5, 5.0, 0.0, 0.1)
+max_price = st.sidebar.slider("Maximim Price", 0 , book['Price'].max(), book['Price'].max())
+
+filtered_book = book.copy()
+
+if selected_author != "ALL":
+    filtered_book = filtered_book[filtered_book['Author'] == selected_author]
+if selected_year != "ALL":
+    filtered_book = filtered_book[filtered_book['Year'] == int(selected_year)]
+if selected_genre != "ALL":
+    filtered_book = filtered_book[filtered_book['Genre'] == selected_genre]
+
+filtered_book = filtered_book[
+    (filtered_book['User Rating'] >= min_rating) & (filtered_book['Price'] <= max_price)
+]
+
+st.subheader("Summary Statistic")
+
+total_books = filtered_book.shape[0]
+unique_titles = filtered_book['Name'].nunique()
+average_rating = filtered_book['User Rating'].mean()
+average_price = filtered_book['Price'].mean()
+
+col1, col2, col3, col4 = st.columns(4)
+col1.metric("Total Books", total_books)
+col2.metric("Titles", unique_titles)
+col3.metric("Average Rating", f"{average_rating:.2f}")
+col4.metric("Average Price", f"${average_price:.2f}")
+
+st.subheader("Dataset preview")
+st.write(filtered_book.head())
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.subheader("Top 10 book Titles")
+    top_titles = filtered_book['Name'].value_counts().head(10)
+    st.bar_chart(top_titles)
+with col2:
+    st.subheader("Top 10 Authors")
+    top_authors = filtered_book['Author'].value_counts().head(10)
+    st.bar_chart(top_authors)
+
+st.subheader("Genre")
+fig = pt.pie(filtered_book, names='Genre', title='Most liked genre(2009-2019)', color='Genre'),
+color_discrete_sequence=pt.colors.sequential.Plasma
+st.plotly_chart(fig[0], use_container_width=True)
